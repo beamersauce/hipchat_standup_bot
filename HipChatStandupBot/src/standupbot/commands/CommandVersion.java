10ae54af7af74a2354a5345366ec56863086002c
cburch <cburch@ikanow.com>
2014-10-25 12:27:55 -0400

    added version command

--- HipChatStandupBot/src/standupbot/commands/CommandVersion.java
+++ HipChatStandupBot/src/standupbot/commands/CommandVersion.java
@@ -1,7 +1,10 @@
 package standupbot.commands;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 import standup.BotData;
 import standup.StandupBot;
@@ -32,6 +35,27 @@ public class CommandVersion extends StandupCommand
 	@Override
 	public String getDisplayMessage(StandupBot bot, BotData botData)
 	{
-		return "Version: TODO";
+		String version = readVersionNumber();
+		return "Version: " + version;
+	}
+	
+	private static final String version_file = "dist/version/Version.properties";
+	private String readVersionNumber()
+	{		
+		String version = null;
+		Properties props = new Properties();
+		InputStream is = null;
+		try
+		{
+			is = new FileInputStream(version_file);
+			props.load(is);
+			version = props.getProperty("version");
+		}
+		catch (Exception ex)
+		{
+			ex.printStackTrace();
+			version = "error reading version file: " + ex.getMessage();
+		}
+		return version;
 	}
 }
